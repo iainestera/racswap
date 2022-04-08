f429d737deb2004ea81dac90f4905ad518cb777d
ChinaNFT <79497730+iainestera@users.noreply.github.com>
2022-04-08 16:31:14 +0800

    Update meta.ts

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -2,9 +2,9 @@ import { ContextApi } from 'contexts/Localization/types';
 import { PageMeta } from './types';
 
 export const DEFAULT_META: PageMeta = {
-  title: 'PancakeSwap',
+  title: 'RACSwap',
   description:
-    'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust.',
+    'The most popular AMM on BSC by user count! Earn rac through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust.',
   image: 'https://pancakeswap.finance/images/hero.png',
 };
 
@@ -12,47 +12,47 @@ export const getCustomMeta = (path: string, t: ContextApi['t']): PageMeta => {
   switch (path) {
     case '/':
       return {
-        title: `${t('Home')} | ${t('PancakeSwap')}`,
+        title: `${t('Home')} | ${t('RACSwap')}`,
       };
     case '/competition':
       return {
-        title: `${t('Trading Battle')} | ${t('PancakeSwap')}`,
+        title: `${t('Trading Battle')} | ${t('RACSwap')}`,
       };
     case '/prediction':
       return {
-        title: `${t('Prediction')} | ${t('PancakeSwap')}`,
+        title: `${t('Prediction')} | ${t('RACSwap')}`,
       };
     case '/farms':
       return {
-        title: `${t('Farms')} | ${t('PancakeSwap')}`,
+        title: `${t('Farms')} | ${t('RACSwap')}`,
       };
     case '/pools':
       return {
-        title: `${t('Pools')} | ${t('PancakeSwap')}`,
+        title: `${t('Pools')} | ${t('RACSwap')}`,
       };
     case '/lottery':
       return {
-        title: `${t('Lottery')} | ${t('PancakeSwap')}`,
+        title: `${t('Lottery')} | ${t('RACSwap')}`,
       };
     case '/collectibles':
       return {
-        title: `${t('Collectibles')} | ${t('PancakeSwap')}`,
+        title: `${t('Collectibles')} | ${t('RACSwap')}`,
       };
     case '/ifo':
       return {
-        title: `${t('Initial Farm Offering')} | ${t('PancakeSwap')}`,
+        title: `${t('Initial Farm Offering')} | ${t('RACSwap')}`,
       };
     case '/teams':
       return {
-        title: `${t('Leaderboard')} | ${t('PancakeSwap')}`,
+        title: `${t('Leaderboard')} | ${t('RACSwap')}`,
       };
     case '/profile/tasks':
       return {
-        title: `${t('Task Center')} | ${t('PancakeSwap')}`,
+        title: `${t('Task Center')} | ${t('RACSwap')}`,
       };
     case '/profile':
       return {
-        title: `${t('Your Profile')} | ${t('PancakeSwap')}`,
+        title: `${t('Your Profile')} | ${t('RACSwap')}`,
       };
     default:
       return null;
